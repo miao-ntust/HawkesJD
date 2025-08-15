001680ecb516e2cc84980189412476657e90a31c
miao-ntust <miao.ntust@gmail.com>
2025-08-14 23:42:24 -0500

    Update example1.m

--- Code/example1.m
+++ Code/example1.m
@@ -1,12 +1,15 @@
-r       = 2.5;
-a       = 1;
-b       = 2;   
-t       = 1;
-lambda0 = 8;
-N       = 20;
+% Hawkes PMF params
+r       = 2.5;   
+a       = 1;     
+b       = 2;     
+t       = 1;    
+lambda0 = 8;     % initial intensity
+N       = 20;    % print probabilities for counts 0..N
 
+% get PMFs from Hawkes
 [P, Pc] = hawkesPMF(r, a, b, t, lambda0, N);
 
+% show results
 format long g
 n = (0:N)';
 
@@ -15,7 +18,7 @@ for i = 1:numel(n)
     fprintf('n=%2d\tPc=%.12g\n', n(i), Pc(i));
 end
 
-fprintf('P (N_t = n):\n');
+fprintf('\nP (N_t = n):\n');
 for i = 1:numel(n)
     fprintf('n=%2d\tP=%.12g\n', n(i), P(i));
 end
